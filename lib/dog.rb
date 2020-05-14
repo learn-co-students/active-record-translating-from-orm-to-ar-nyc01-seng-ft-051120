c8d66307f314bf1ff6ef6548d77375b9536b0dd7
dolly-d <dolly.desir@gmail.com>
2020-05-13 21:34:23 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,12 @@
-class Dog
-end
+require 'bundler/setup'
+Bundler.require
+require 'rake'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "./db/dogs.db" )
+
+  class Dog < ActiveRecord::Base
+    
+  end
